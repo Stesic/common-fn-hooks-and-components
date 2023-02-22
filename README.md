c259152cc3cba3d469e858db882027f26b309563
Stesic <ivan.stesic@gmail.com>
2023-02-22 20:09:30 +0100

    update

--- README.md
+++ README.md
@@ -1,16 +1,1 @@
-# most-common-functions
-
-const useOnUnmount = (callback, dependencies) => {
-  const isUnmounting = React.useRef(false);
-
-  useEffect(() => () => (isUnmounting.current = true), []);
-
-  useEffect(
-    () => () => {
-      if (isUnmounting.current) {
-        callback();
-      }
-    },
-    dependencies,
-  );
-};
+# most-common
---
+++ componenents/Accordion.tsx
@@ -0,0 +1,68 @@
+import { FC, ReactNode, useRef, useState } from "react";
+
+type TAccordion = {
+  children: ReactNode;
+  label: string;
+};
+const Accordion: FC<TAccordion> = ({ children, label }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const panelRef = useRef() as any;
+
+  const toggleOpen = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <>
+      <Headline onClick={toggleOpen}>
+        <span>{label}</span>
+        <IconWrap isOpen={isOpen}>{Icons.ArrowDown}</IconWrap>
+      </Headline>
+      <Panel
+        isOpen={isOpen}
+        ref={panelRef}
+        height={
+          panelRef?.current ? panelRef?.current?.scrollHeight + "px" : "auto"
+        }
+      >
+        {children}
+      </Panel>
+    </>
+  );
+};
+
+export default Accordion;
+
+const Headline = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  padding: 15px 20px;
+  width: 100%;
+  gap: 5px;
+  justify-content: space-between;
+  background: var(--midnight-green);
+
+  box-shadow: 0px 8px 11px -3px rgba(0, 0, 0, 0.08);
+  border-radius: 10px;
+  cursor: pointer;
+`;
+
+const IconWrap = styled.div<{ isOpen: boolean }>`
+  display: flex;
+  align-items: center;
+  svg {
+    transform: ${(props) => (props.isOpen ? "rotate(180deg)" : "none")};
+    transition: all 0.2s ease-in-out;
+    path {
+      fill: var(--primary-green);
+    }
+  }
+`;
+
+const Panel = styled.div<{ isOpen: boolean; height: string }>`
+  max-height: ${({ isOpen, height }) => (isOpen ? height : "0px")};
+  opacity: ${({ isOpen }) => (isOpen ? "1" : "0")};
+  overflow: hidden;
+  transition: all 0.2s ease-in-out;
+
+  width: 100%;
+`;
